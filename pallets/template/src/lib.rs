bdc076806be9e157e897ebb63d0366ba2faf0809
Li Smith <jinrishouji@sina.com>
2022-06-08 22:42:37 +0800

    add_price_check

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -66,6 +66,7 @@ pub mod pallet {
 	use frame_support::traits::UnixTime;
 	use frame_support::{ensure, pallet_prelude::*};
 	use frame_system::pallet_prelude::*;
+	use lite_json::json::JsonValue;
 	use sp_runtime::traits::One;
 	use sp_runtime::{
 		offchain::{
@@ -348,17 +349,26 @@ pub mod pallet {
 							}
 						},
 						Some(Triger::PriceGT(_, price)) => {
-							//todo(check price gt)
-							(*recipe).times += 1;
-							(*recipe).done = true;
+							let price = Self::fetch_price().map_err(|_| "Failed to fetch price")?;
 
-							map_running_action_recipe_task.insert(*recipe_id, recipe.clone());
+							if (price < fetch_price) {
+								(*recipe).times += 1;
+								(*recipe).done = true;
+
+								map_running_action_recipe_task.insert(*recipe_id, recipe.clone());
+							}
 						},
 						Some(Triger::PriceLT(_, price)) => {
+							let fetch_price =
+								Self::fetch_price().map_err(|_| "Failed to fetch price")?;
+
 							//todo(check price gt)
-							(*recipe).times += 1;
-							(*recipe).done = true;
-							map_running_action_recipe_task.insert(*recipe_id, recipe.clone());
+							if (price > fetch_price) {
+								(*recipe).times += 1;
+								(*recipe).done = true;
+
+								map_running_action_recipe_task.insert(*recipe_id, recipe.clone());
+							}
 						},
 						_ => {},
 					}
@@ -384,7 +394,85 @@ pub mod pallet {
 		}
 	}
 
-	impl<T: Config> Pallet<T> {}
+	impl<T: Config> Pallet<T> {
+		/// Fetch current price and return the result in cents.
+		fn fetch_price() -> Result<u32, http::Error> {
+			let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(2_000));
+			// Initiate an external HTTP GET request.
+			// This is using high-level wrappers from `sp_runtime`, for the low-level calls that
+			// you can find in `sp_io`. The API is trying to be similar to `reqwest`, but
+			// since we are running in a custom WASM execution environment we can't simply
+			// import the library here.
+			let request = http::Request::get(
+				"https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD",
+			);
+
+			// We set the deadline for sending of the request, note that awaiting response can
+			// have a separate deadline. Next we send the request, before that it's also possible
+			// to alter request headers or stream body content in case of non-GET requests.
+			let pending = request.deadline(deadline).send().map_err(|_| http::Error::IoError)?;
+
+			// The request is already being processed by the host, we are free to do anything
+			// else in the worker (we can send multiple concurrent requests too).
+			// At some point however we probably want to check the response though,
+			// so we can block current thread and wait for it to finish.
+			// Note that since the request is being driven by the host, we don't have to wait
+			// for the request to have it complete, we will just not read the response.
+			let response =
+				pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
+			// Let's check the status code before we proceed to reading the response.
+			if response.code != 200 {
+				log::warn!("Unexpected status code: {}", response.code);
+				return Err(http::Error::Unknown);
+			}
+
+			// Next we want to fully read the response body and collect it to a vector of bytes.
+			// Note that the return object allows you to read the body in chunks as well
+			// with a way to control the deadline.
+			let body = response.body().collect::<Vec<u8>>();
+
+			// Create a str slice from the body.
+			let body_str = sp_std::str::from_utf8(&body).map_err(|_| {
+				log::warn!("No UTF8 body");
+				http::Error::Unknown
+			})?;
+
+			log::info!("fetch_price: {}", body_str);
+
+			let price = match Self::parse_price(body_str) {
+				Some(price) => Ok(price),
+				None => {
+					log::warn!("Unable to extract price from the response: {:?}", body_str);
+					Err(http::Error::Unknown)
+				},
+			}?;
+
+			log::info!("Got price: {} cents", price);
+
+			Ok(price)
+		}
+
+		/// Parse the price from the given JSON string using `lite-json`.
+		///
+		/// Returns `None` when parsing failed or `Some(price in cents)` when parsing is successful.
+		fn parse_price(price_str: &str) -> Option<u32> {
+			let val = lite_json::parse_json(price_str);
+			let price = match val.ok()? {
+				JsonValue::Object(obj) => {
+					let (_, v) =
+						obj.into_iter().find(|(k, _)| k.iter().copied().eq("USD".chars()))?;
+					match v {
+						JsonValue::Number(number) => number,
+						_ => return None,
+					}
+				},
+				_ => return None,
+			};
+
+			let exp = price.fraction_length.checked_sub(2).unwrap_or(0);
+			Some(price.integer as u32 * 100 + (price.fraction / 10_u64.pow(exp)) as u32)
+		}
+	}
 
 	impl<T: Config> BlockNumberProvider for Pallet<T> {
 		type BlockNumber = T::BlockNumber;
